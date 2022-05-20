4d64d6b2cd72cb794e900a80ef021a543f4003ac
javiinostroza <jminostroza@uc.cl>
2022-05-20 19:35:26 -0400

    modify loggedHeader

--- MyAnimes/src/components/LoggedHeader.vue
+++ MyAnimes/src/components/LoggedHeader.vue
@@ -1,50 +1,47 @@
 <script setup>
+import router from "./../router";
 </script>
 
 <template>
-    <div>
-        <h3>Home</h3>
-        <h5>Grupo 05 - Tarea 5, Vue JS</h5>
-        <p> Javiera Inostroza </p>
-        <p> Elías Sabja </p>
-        <p> Samuel Zúñiga </p>
-        <p class="links"><a href="https://github.com/IIC3585-2022/vue-grupo-05">Repositorio Github</a></p>
-    </div>
+    <ul>
+        <li><router-link to="/" class="logo">Home</router-link></li>
+        <li class="my-animes"><a href="#log-in">My Animes</a></li>
+        <li><router-link to="/About" class="about">About</router-link></li>
+        <li class="log-user"><a  href="#unlogged-home">Log Out</a></li>
+    </ul>
 </template>
 
 <style scoped>
-    div, h3, h5, p, a {
-        float: center;
-    }
-
-    h3, h5, p {
-    color: white;
-    } 
-
-    h3 {
-        font-size: 5rem;
-    }
-
-    h5 {
-        font-size: 1.5rem;
-        padding-top: 50px;
-        padding-bottom: 50px;
-    }
-
-    a {
-        color: #fd9330;
-    }
-
-    div {
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
         background-color: #333;
+    }
+
+    li {
+        float: left;
+    }
+
+    li a {
+        display: block;
+        color: white;
         text-align: center;
-        padding-top: 100px;
+        padding: 14px 16px;
+        text-decoration: none;
     }
 
-    .links {
-        padding-top: 20px;
+    li a:hover:not(.active) {
+        background-color: #111;
     }
 
-
-
+    .active {
+        background-color: #111;
+    }
+    
+    .log-user {
+        float: right;
+    }
 </style>
+
